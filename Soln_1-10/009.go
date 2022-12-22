e832263229cd17ec523e5918ec7d11dd7241eb67
nemzyxt <bR0S3@protonmail.com>
2022-12-22 22:35:22 +0300

    Format properly with go-fmt

--- Soln_1-10/009.go
+++ Soln_1-10/009.go
@@ -7,14 +7,14 @@ import (
 
 func isPythagoreanTriplet(a, b, c int) bool {
 	x, y, z := float64(a), float64(b), float64(c)
-	return (math.Pow(x,2) + math.Pow(y,2)) == math.Pow(z,2)
+	return (math.Pow(x, 2) + math.Pow(y, 2)) == math.Pow(z, 2)
 }
 
 func main() {
 	ans := 0
-	for a:=1; a<1000; a++ {
-		for b:=(a+1); b<1000; b++ {
-			c := 1000-(a+b)
+	for a := 1; a < 1000; a++ {
+		for b := (a + 1); b < 1000; b++ {
+			c := 1000 - (a + b)
 			if isPythagoreanTriplet(a, b, c) {
 				ans = a * b * c
 			}
