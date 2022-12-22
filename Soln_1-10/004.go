037b821d4bcef7141687cbb4f86baaad8038a4c3
nemzyxt <bR0S3@protonmail.com>
2022-12-22 21:41:32 +0300

    Add solution 4 code

--- Soln_1-10/004.go
+++ Soln_1-10/004.go
@@ -2,22 +2,35 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 func reverseNumber(n int) int {
-	num := strconv.Itoa(n)
-	digits := len(num)
-	for i := (digits - 1); i > 0; i-- {
-
+	rev_num := 0
+	for n > 0 {
+		rev_num = rev_num*10 + n%10
+		n /= 10
 	}
+	return rev_num
 }
 
 func isPalindrome(n int) bool {
-
+	if reverseNumber(n) == n {
+		return true
+	}
+	return false
 }
 
 func main() {
 	ans := 0
+	for i := 999; i >= 100; i-- {
+		for j := 999; j >= 100; j-- {
+			tmp := i * j
+			if isPalindrome(tmp) && i != j {
+				fmt.Println(i, " ", j)
+				ans = tmp
+				break
+			}
+		}
+	}
 	fmt.Printf("Answer : %d\n", ans)
 }
