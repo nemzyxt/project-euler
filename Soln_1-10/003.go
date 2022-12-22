49683616c781102db7355a6dda51d652d057f64b
nemzyxt <bR0S3@protonmail.com>
2022-12-22 19:41:41 +0300

    Add solution for problem 3

---
+++ Soln_1-10/003.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func isPrime(n int64) bool {
+	var i int64 = 2
+	for ; i < n; i++ {
+		if n%i == 0 { return false }
+	}
+	return true
+}
+
+func main() {
+	ans := 0
+	var num int64 = 600851475143
+	var x int64 = int64(math.Sqrt(float64(num)))
+	for ; x > 1; x-- {
+		if num%x == 0 && isPrime(x) {
+			ans = int(x)
+			break
+		}
+	}
+	fmt.Printf("Answer : %d\n", ans)
+}
